cbb6156a1c4109733fbab55c447446abf7d9396a
Karens05 <144145811+Karens05@users.noreply.github.com>
2024-11-18 21:16:11 -0600

    Update login.php

--- login.php
+++ login.php
@@ -1,1 +1,13 @@
+<?php
+include 'db.php';
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email_usuario'];
+    $password = $_POST['contrasena_usuario'];
+
+    // Conexión a la base de datos
+    $conexion = new mysqli('localhost', 'root', '', 'pasteleria_k&a');
+
+    if ($conexion->connect_error) {
+        die("Error de conexión: " . $conexion->connect_error);
+    }
