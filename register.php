92ea628d7a6b7ada69f45563aeb2ad6ff692e740
Karens05 <144145811+Karens05@users.noreply.github.com>
2024-11-18 21:15:26 -0600

    Update register.php

--- register.php
+++ register.php
@@ -1,1 +1,17 @@
+<?php
+include 'db.php';
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST['nombre_usuario'];
+    $apellido = $_POST['apellido_usuario'];
+    $email = $_POST['email_usuario'];
+    $password = $_POST['contrasena_usuario'];
+
+    // Conexión a la base de datos
+    $conexion = new mysqli('localhost', 'root', '', 'pasteleria_k&a');
+
+    if ($conexion->connect_error) {
+        die("Error de conexión: " . $conexion->connect_error);
+    }
+
+   
